e20fc9309a0de473c30020d46fd9fd68bd15c658
eriksaputra <erikayi46@gmail.com>
2019-09-05 10:42:23 +0700

    tanggal

---
+++ php/date2.php
@@ -0,0 +1,8 @@
+<?php
+// Sekarang tanggal 13 April 2006 09:43:49
+$arrDay = array("Minggu", "Senin", "Selasa", "Rabu", "Kamis",
+"Jum'at", "Sabtu");
+$day = date ("w");
+//0 - 6 of day
+echo "Alhamdullah udah hari : <b>" . $arrDay[$day]."</b>";
+?>
